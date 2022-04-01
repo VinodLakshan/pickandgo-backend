8ee6c4a66e9f0b88fc621b8232799e0b9e0b0c0f
Zayd Ahmed <zaidthahir879@Gmail.com>
2022-04-01 17:50:26 +0530

    Commit

--- src/main/java/edu/esoft/sdp/pickAndGoBackend/service/impl/VehicleServiceImpl.java
+++ src/main/java/edu/esoft/sdp/pickAndGoBackend/service/impl/VehicleServiceImpl.java
@@ -1,5 +1,7 @@
 package edu.esoft.sdp.pickAndGoBackend.service.impl;
 
+import edu.esoft.sdp.pickAndGoBackend.model.Branch;
+import edu.esoft.sdp.pickAndGoBackend.model.Vehicle;
 import edu.esoft.sdp.pickAndGoBackend.repository.VehicleRepository;
 import edu.esoft.sdp.pickAndGoBackend.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,4 +18,17 @@ public class VehicleServiceImpl implements VehicleService {
     public List<?> getAllVehicle() {
         return vehicleRepository.findAll();
     }
+
+    @Override
+    public Vehicle createVehicle(Vehicle vehicle) {
+        return vehicleRepository.save(vehicle);
+    }
+
+    @Override
+    public String deleteVehicle(Integer id) {
+        vehicleRepository.deleteById(id);
+        return "Branch with ID " + id + " Has Been Removed Successfully";
+    }
+
+
 }
